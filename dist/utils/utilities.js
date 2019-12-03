37dd76c4c1d7a0a37e90a8d10cfd19d4998b12c5
josemak25 <amakirij@gmail.com>
2019-12-03 12:46:02 +0100

    add dist to git ignore file

--- dist/utils/utilities.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.constructError = function (error) {
-    return error.reduce(function (acc, err) {
-        acc[err.path[0]] = err.message.replace(/"/g, '');
-        return acc;
-    }, {});
-};
-//# sourceMappingURL=utilities.js.map
