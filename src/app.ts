2c57868916464a49a509f737c2b7cb4360342c41
josemak25 <amakirij@gmail.com>
2019-12-08 08:20:17 +0100

    add setup for app start up

--- src/app.ts
+++ src/app.ts
@@ -1,41 +1,63 @@
-import createError from 'http-errors';
-import express, { Response, Request, NextFunction } from 'express';
-import path from 'path';
-import cookieParser from 'cookie-parser';
-import logger from 'morgan';
-
-import indexRouter from './routes/index';
-import courseRouter from './routes/course';
+import express from "express";
+import logger from "morgan";
+import bodyParser from "body-parser";
+import cookieParser from "cookie-parser";
+import compress from "compression";
+import methodOverride from "method-override";
+import cors from "cors";
+import expressWinston from "express-winston";
+import helmet from "helmet";
+import winstonInstance from "./config/winston";
+import error from "./config/errors";
+const config = require("./config/env");
+import routes from "./routes/index";
 
 const app = express();
-
-// view engine setup
-app.set('views', path.join(__dirname, '../views'));
-app.set('view engine', 'jade');
-
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+// secure apps by setting various HTTP headers
+app.use(helmet());
+app.use(cors({ credentials: true, origin: true }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, '../public')));
 
-app.use('/', indexRouter);
-app.use('/api/course', courseRouter);
+// parse body params and attach them to res.body
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(compress());
+app.use(methodOverride());
+
+// enable detailed API logging in dev env
+if (config.env === "development") {
+  app.use(logger("dev"));
+  expressWinston.responseWhitelist.push("body");
+  app.use(
+    expressWinston.logger({
+      winstonInstance,
+      meta: true, // optional: log meta data about request (defaults to true)
+      msg:
+        "HTTP {{req.method}} {{req.url}} {{res.statusCode}} {{res.responseTime}}ms",
+      colorStatus: true // Color the status code (default green, 3XX cyan, 4XX yellow, 5XX red).
+    })
+  );
+}
+
+// all routes are marked as private routes within the app
+app.use("/api/v1", routes);
+
+// if error is not an instanceOf APIError, convert it.
+app.use(error.converter);
 
 // catch 404 and forward to error handler
-app.use(function(_req, _res, next) {
-  next(createError(404));
-});
+app.use(error.notFound);
 
-// error handler
-app.use(function(err: any, req: Request, res: Response, _next: NextFunction) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+// error handler, send stacktrace only during development
+app.use(error.handler);
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
+if (config.env !== "test") {
+  app.use(
+    expressWinston.errorLogger({
+      winstonInstance
+    })
+  );
+}
 
 export default app;
