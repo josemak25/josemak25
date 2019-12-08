e47175ae0558b6a540feecf2256416a85bd3ace8
josemak25 <amakirij@gmail.com>
2019-12-08 08:25:00 +0100

    add route handler for test user and service health-check

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,9 +1,18 @@
-import { Request, Response, NextFunction, Router } from 'express';
+import { Request, Response, Router } from "express";
+
+import userRoutes from "./user.route";
+
 const router = Router();
 
-/* GET home page. */
-router.get('/', function(_req: Request, res: Response, _next: NextFunction) {
-  res.render('index', { title: 'Express' });
-});
+/** GET /health-check - Check service health */
+router.get("/health-check", (_req: Request, res: Response) =>
+  res.send("We're Good")
+);
+
+// //mount auth routes
+// router.use("/auth", authRoutes);
+
+//mount users routes
+router.use("/users", userRoutes);
 
 export default router;
