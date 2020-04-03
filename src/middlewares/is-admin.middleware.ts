ab31267011e2e061b59c0a1db5215eb265665960
josemak25 <amakirij@gmail.com>
2020-04-03 11:08:55 +0100

    add is admin middleware

---
+++ src/middlewares/is-admin.middleware.ts
@@ -0,0 +1,21 @@
+import { Request, Response, NextFunction } from 'express';
+import httpStatus from 'http-status';
+import sendResponse from '../helpers/response';
+import { Role } from '../types/user';
+
+export default (req: Request, res: Response, next: NextFunction) => {
+  const { role } = req.token;
+
+  if (role !== Role.admin) {
+    return res.json(
+      sendResponse(
+        httpStatus.UNAUTHORIZED,
+        'You are not Authorized to perform this operation!',
+        null,
+        { error: 'Invalid credentials' }
+      )
+    );
+  }
+
+  return next();
+};
